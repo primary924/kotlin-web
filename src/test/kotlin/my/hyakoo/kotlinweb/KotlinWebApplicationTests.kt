c75a08b779781bf5b939171ebe24a4211fc1c554
yonghyucklim <hyakoo@yonghyucklim.local>
2023-11-30 13:56:24 +0900

    initial commit

---
+++ src/test/kotlin/my/hyakoo/kotlinweb/KotlinWebApplicationTests.kt
@@ -0,0 +1,13 @@
+package my.hyakoo.kotlinweb
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinWebApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
